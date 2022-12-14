3e68f7cbf3d4e3fe0fba23176c760a82118bd0d5
Ecaterina <ecaterinacoseru@gmail.com>
2022-12-14 15:04:01 -0500

    US05 most popular book genre

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -2,3 +2,9 @@ select * from books;
 
 select count(*) as borrowedBooks from users u
 inner join book_borrow b on u.id = b.user_id where is_returned = 0;
+
+select bc.name,count(*) from book_borrow bb
+                                 inner join books b on bb.book_id = b.id
+                                 inner join book_categories bc on b.book_category_id = bc.id
+                                 group by bc.name
+                                 order by 2 desc;
