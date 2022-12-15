675d575838577b47dd308c9583e812494d108bf8
tolga bolen <tolgabolen@hotmail.com>
2022-12-14 21:02:22 -0500

    US04 created

--- src/test/resources/queries/library.sql
+++ src/test/resources/queries/library.sql
@@ -1,8 +1,12 @@
-select * from books
-where name= 'Chordeiles minor';
+select count(*) as Total_Number_Of_Librarian from users
+where user_group_id = 2 and status = 'Active';
 
-select name,isbn,author,description,year from books where name='Chordeiles minor'
+select count(*) as borrowedBooks from users u
+inner join book_borrow b on u.id = b.user_id where is_returned = 0;
 
-
-
+select bc.name,count(*) from book_borrow bb
+                                 inner join books b on bb.book_id = b.id
+                                 inner join book_categories bc on b.book_category_id = bc.id
+                                 group by bc.name
+                                 order by 2 desc;
 
